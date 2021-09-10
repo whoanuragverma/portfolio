b72a7d0636fe03d00143f9f0200de2d7078f3496
Anurag Verma <anu.anuragverma@gmail.com>
2021-09-10 18:24:24 +0000

    🔨 Added Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/globals.css';
-
+import Script from 'next/script';
 function MyApp({ Component, pageProps }): JSX.Element {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Script
+                strategy="lazyOnload"
+                id="main"
+                src="https://www.googletagmanager.com/gtag/js?id=G-QZR2HQVPER"
+            />
+            <Script strategy="lazyOnload" id="loader">
+                {`window.dataLayer = window.dataLayer || [];
+                          function gtag(){dataLayer.push(arguments);}
+                          gtag('js', new Date());
+                          gtag('config', 'G-QZR2HQVPER');`}
+            </Script>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
